4c17f8ee0472b8e09feb46bf2de59cd50fd8a1b0
Romeo-04 <147164919+Romeo-04@users.noreply.github.com>
2025-06-23 19:06:48 +0800

    Use password hashing

--- login.php
+++ login.php
@@ -2,25 +2,28 @@
 
 include 'connection_login.php';
 
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$stmt = $conn->prepare("SELECT * FROM username WHERE username = ? AND password = ?");
-$stmt->bind_param("ss", $username, $password);
+$stmt = $conn->prepare("SELECT password FROM username WHERE username = ?");
+$stmt->bind_param("s", $username);
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    session_start();
-    header("Location: landing_page.php");
-    exit();
-
-} else {
-
-    header("Location: index.php?error=Invalid username or password");
-    exit();
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row['password'])) {
+        session_start();
+        $stmt->close();
+        $conn->close();
+        header("Location: landing_page.php");
+        exit();
+    }
 }
 
 $stmt->close();
 $conn->close();
+header("Location: index.php?error=Invalid username or password");
+exit();
 ?>
